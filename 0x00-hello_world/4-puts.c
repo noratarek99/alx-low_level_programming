a15150cf4db034881540a9e9dabd25d6821d3c3b
noratarek99 <tareknora130@gmail.com>
2023-09-08 14:44:04 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A cProg. prints puts
+ *
+ * Return: Always 0 (sucsses)
+ *
+ */
+
+
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
